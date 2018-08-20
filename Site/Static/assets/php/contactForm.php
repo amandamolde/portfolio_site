728fa7ff9831508b1fd1bbb30bdf7118c39e7554
amandamolde <amanda.molde@gmail.com>
2018-08-19 20:30:10 -0600

    updated email in contact form

--- Site/Static/assets/php/contactForm.php
+++ Site/Static/assets/php/contactForm.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Contact
-	$to = 'amanda.molde@gmail.com;
+	$to = 'amanda@amandamolde.com;
 	$subject = 'Subject here...';
 
 	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_message'])){
